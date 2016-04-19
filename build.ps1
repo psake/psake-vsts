9e414873bcfd880e53a302abd68abbfe3374ea83
Guillaume Rouchon <guillaume.rouchon@rogui.net>
2016-04-19 08:17:23 +0200

    Update build script to support VSTS build number

--- build.ps1
+++ build.ps1
@@ -39,6 +39,15 @@ Task Clean {
     }
 }
 
+Task UpdateBuildNumber {
+    # set build number to extension version
+    $path = Join-Path $buildPath 'vss-extension.json'
+    $metadata = Get-Content $path -Raw | ConvertFrom-Json
+    
+    Write-Host "##vso[build.updatebuildnumber]psake-extension_$($metadata.version)+${env:BUILD_BUILDID}"
+    Write-Host "##vso[task.setvariable variable=Extension.Version;]$($metadata.version)"
+}
+
 Task PublishDev {
     # check for build directory
     if (!(Test-Path $buildPath))
@@ -46,61 +55,98 @@ Task PublishDev {
         throw "Directory '${buildPath}' not found."
     }
     
-    # generate unique version
+    # generate unique version:
+    #  - major: 0
+    #  - minor: number of days since 2000-01-01
+    #  - patch: number of seconds since midnight divided by 2
     $ref = New-Object DateTime 2000,1,1
-    $now = [DateTime]::Now
+    $now = [DateTime]::UtcNow
     $major = 0
     $minor = $now.Subtract($ref).Days
     $patch = [Math]::Floor([Math]::Floor($now.TimeOfDay.TotalSeconds) * 0.5)
     $version = "${major}.${minor}.${patch}"
     
-    # update extension metadata
-    $extensionPath = Join-Path $buildPath 'vss-extension.json'
-    $extensionMetadata = Get-Content $extensionPath -Raw | ConvertFrom-Json
-    
-    Write-Host "Adding tag '-dev' to extension id..."
-    $extensionMetadata.id = $extensionMetadata.id -replace '#{IdTag}','-dev'
-    
-    Write-Host "Adding tag ' (dev)' to extension name..."
-    $extensionMetadata.name = $extensionMetadata.name -replace '#{NameTag}',' (dev)'
-    
-    Write-Host "Updating extension version to '${version}'..."
-    $extensionMetadata.version = $version
-
-    if ($extensionMetadata.public)
-    {
-        Write-Host 'Removing extension public flag...'
-        $extensionMetadata = $extensionMetadata | Select-Object -Property * -ExcludeProperty 'public'
-    }
-    
-    Write-Host "Updating extension file '${extensionPath}'..."
-    ConvertTo-Json $extensionMetadata -Depth 20 | Out-File $extensionPath -Encoding utf8 -Force
-    
-    # update task metadata
-    $taskPath = Join-Path $buildPath 'task\task.json'
-    $taskMetadata = Get-Content $taskPath -Raw | ConvertFrom-Json
-
-    Write-Host "Updating task id to '7011cf27-c181-443a-9f07-3e6ffea72b6b'..."
-    $taskMetadata.id = $taskMetadata.id -replace  '#{Task.Id}','7011cf27-c181-443a-9f07-3e6ffea72b6b'
-    
-    Write-Host "Adding tag '-dev' to task name..."
-    $taskMetadata.name = $taskMetadata.name -replace '#{IdTag}','-dev'
-    
-    Write-Host "Adding tag ' (dev ${version})' to task friendly name..."
-    $taskMetadata.friendlyName = $taskMetadata.friendlyName -replace '#{NameTag}'," (dev ${version})"
-    
-    Write-Host "Updating task version to '${version}'..."
-    $taskMetadata.version.major = 0
-    $taskMetadata.version.minor = $minor
-    $taskMetadata.version.patch = $patch
-    
-    Write-Host "Adding version to task help..."
-    $taskMetadata.helpMarkDown = $taskMetadata.helpMarkDown -replace '#{Task.Version}',$version
-    
-    Write-Host "Updating task file '${taskPath}'..."
-    ConvertTo-Json $taskMetadata -Depth 20 | Out-File $taskPath -Encoding utf8 -Force
+    # update metadata
+    _UpdateExtensionMetaData -IdTag '-dev' -NameTag ' (dev)' -Version $version
+    _UpdateTaskMetadata -Id '7011cf27-c181-443a-9f07-3e6ffea72b6b' -NameTag '-dev' -FriendlyNameTag " (dev ${version})" -Version $version
     
     # publish extension
+    _PublishExtension
+}
+
+Function _UpdateExtensionMetaData
+{
+    param(
+        [string] $IdTag,
+        [string] $NameTag,
+        [string] $Version,
+        [switch] $Public
+    )
+    
+    $path = Join-Path $buildPath 'vss-extension.json'
+    $metadata = Get-Content $path -Raw | ConvertFrom-Json
+    
+    Write-Host "Adding tag '${IdTag}' to extension id..."
+    $metadata.id = $metadata.id -replace '#{IdTag}',$IdTag
+    
+    Write-Host "Adding tag '${NameTag}' to extension name..."
+    $metadata.name = $metadata.name -replace '#{NameTag}',$NameTag
+    
+    Write-Host "Updating extension version to '${Version}'..."
+    $metadata.version = $Version
+
+    if (!$Public -and $metadata.public)
+    {
+        Write-Host 'Removing extension public flag...'
+        $metadata = $metadata | Select-Object -Property * -ExcludeProperty 'public'
+    }
+    
+    if ($Public -and !$metadata.public)
+    {
+        Write-Host 'Adding extension public flag...'
+        Add-Member -InputObject $metadata -NotePropertyName "public" -NotePropertyValue $true -Force
+    }
+    
+    Write-Host "Updating extension file '${path}'..."
+    ConvertTo-Json $metadata -Depth 20 | Out-File $path -Encoding utf8 -Force
+}
+
+Function _UpdateTaskMetadata
+{
+    param(
+        [string] $Id,
+        [string] $NameTag,
+        [string] $FriendlyNameTag,
+        [string] $Version
+    )
+    
+    $path = Join-Path $buildPath 'task\task.json'
+    $metadata = Get-Content $path -Raw | ConvertFrom-Json
+
+    Write-Host "Updating task id to '${Id}'..."
+    $metadata.id = $metadata.id -replace  '#{Task.Id}',$Id
+    
+    Write-Host "Adding tag '${NameTag}' to task name..."
+    $metadata.name = $metadata.name -replace '#{IdTag}',$NameTag
+    
+    Write-Host "Adding tag '${FriendlyNameTag}' to task friendly name..."
+    $metadata.friendlyName = $metadata.friendlyName -replace '#{NameTag}',$FriendlyNameTag
+    
+    Write-Host "Updating task version to '${Version}'..."
+    $parsedVersion = [Version]::Parse($Version)
+    $metadata.version.major = $parsedVersion.Major
+    $metadata.version.minor = $parsedVersion.Minor
+    $metadata.version.patch = $parsedVersion.Build
+    
+    Write-Host "Adding version to task help..."
+    $metadata.helpMarkDown = $metadata.helpMarkDown -replace '#{Task.Version}',$Version
+    
+    Write-Host "Updating task file '${path}'..."
+    ConvertTo-Json $metadata -Depth 20 | Out-File $path -Encoding utf8 -Force
+}
+
+Function _PublishExtension
+{
     Write-Host "Publishing extension..."
     $arguments = @(
         'extension',
